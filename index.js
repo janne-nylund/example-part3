e39e582df23eef2124bfdfe7b0efdbc0653858a9
janne-nylund <janne.nylund@centria.fi>
2020-10-21 10:01:05 +0300

    Add Heroku

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -96,7 +98,6 @@ app.delete('/api/notes/:id', (request, response) => {
 app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
