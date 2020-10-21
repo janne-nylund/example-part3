5a65436aa932e6039304aed7b7a2b2b2a975356a
janne-nylund <janne.nylund@centria.fi>
2020-10-21 10:16:01 +0300

    Add frontend build

---
+++ build/precache-manifest.4cf94ae5026cf745bf66543b91effb69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5c5724f693295f016ee55088dc7a6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69af86d4ed9cfd1c676d",
+    "url": "/static/css/main.8febad61.chunk.css"
+  },
+  {
+    "revision": "b687281982d0621163a2",
+    "url": "/static/js/2.590b4e07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.590b4e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69af86d4ed9cfd1c676d",
+    "url": "/static/js/main.1032ea0c.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
